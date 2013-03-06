ffe317ffeed740d3719423df57593a1d40515f36
Eric Watson <wasnotrice@gmail.com>
2013-03-06 13:28:22 -0600

    Refactor Furoshiki::Shoes.new

--- lib/furoshiki/shoes.rb
+++ lib/furoshiki/shoes.rb
@@ -1,12 +1,12 @@
 module Furoshiki
   module Shoes
     def self.new(backend, wrapper, config)
-      backend_class_name = backend.to_s.capitalize
-      wrapper_class_name = wrapper.to_s.capitalize
-      klass = self.const_get("#{backend_class_name}#{wrapper_class_name}")
-      klass.new config
-    rescue LoadError => e
-      raise LoadError, "Couldn't load backend '#{backend}'. Error: #{e.message}\n#{e.backtrace.join("\n")}"
+      class_name = class_name_for(backend, wrapper)
+      self.const_get(class_name).new(config)
+    end
+
+    def self.class_name_for(backend, wrapper)
+      [backend, wrapper].map { |name| name.to_s.capitalize }.join
     end
   end
 end
