9d9836238fb8dd70bdc986c65924510cffd1c3c0
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-03-31 19:08:58 +0200

    Getting rid of all the rspec deprecation warnings.
    * the warnings were concerning usage of let/subject in before :all
    * approach is to substitute them with instance variables/local
      vars
    * not perfect but working for now, feel free to give feedback =)
    * fixes #2

--- spec/shoes/swt_jar_spec.rb
+++ spec/shoes/swt_jar_spec.rb
@@ -11,17 +11,18 @@ describe Furoshiki::Shoes::SwtJar do
 
   context "when creating a .jar" do
     before :all do
-      output_dir.rmtree if output_dir.exist?
-      output_dir.mkpath
-      Dir.chdir app_dir do
-        @jar_path = subject.package(output_dir)
+      @output_dir.rmtree if @output_dir.exist?
+      @output_dir.mkpath
+      config = Furoshiki::Shoes::Configuration.load(@config_filename)
+      @subject = Furoshiki::Shoes::SwtJar.new(config)
+      Dir.chdir @app_dir do
+        @jar_path = @subject.package(@output_dir)
       end
     end
 
     let(:jar_name) { 'sweet-nebulae.jar' }
-    let(:output_file) { Pathname.new(output_dir.join jar_name) }
-    let(:config) { Furoshiki::Shoes::Configuration.load(config_filename) }
-    subject { Furoshiki::Shoes::SwtJar.new(config) }
+    let(:output_file) { Pathname.new(@output_dir.join jar_name) }
+    subject { @subject }
 
     it "creates a .jar" do
       output_file.should exist
@@ -40,7 +41,7 @@ describe Furoshiki::Shoes::SwtJar do
       jar.entries.should_not include("dir_to_ignore/file_to_ignore")
     end
 
-    its(:default_dir) { should eq(output_dir) }
+    its(:default_dir) { should eq(@output_dir) }
     its(:filename) { should eq(jar_name) }
   end
 
--- spec/zip/directory_contents_spec.rb
+++ spec/zip/directory_contents_spec.rb
@@ -3,22 +3,27 @@ require 'fileutils'
 require 'furoshiki/zip'
 
 describe Furoshiki::Zip::DirectoryContents do
-  subject { Furoshiki::Zip::DirectoryContents.new input_dir, output_file }
 
   context "output file" do
     include_context 'zip'
 
+    before :all do
+      zip_directory_contents = Furoshiki::Zip::DirectoryContents.new input_dir, @output_file
+      zip_directory_contents.write
+      @zip = Zip::ZipFile.open @output_file
+    end
+
     it "exists" do
-      output_file.should exist
+      @output_file.should exist
     end
 
     it "does not include input directory without parents" do
-      zip.entries.map(&:name).should_not include(add_trailing_slash input_dir.basename)
+      @zip.entries.map(&:name).should_not include(add_trailing_slash input_dir.basename)
     end
 
     relative_input_paths(input_dir).each do |path|
       it "includes all children of input directory" do
-        zip.entries.map(&:name).should include(path)
+        @zip.entries.map(&:name).should include(path)
       end
     end
   end
