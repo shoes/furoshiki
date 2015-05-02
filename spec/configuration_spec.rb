aef8fb24670e006e24839a3f556a6ef11adc6e5a
Jason R. Clark <jclark@newrelic.com>
2015-05-01 22:07:45 -0700

    Spec for config exclusions using proper application path

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -2,11 +2,27 @@ require 'spec_helper'
 require 'furoshiki/configuration'
 
 describe Furoshiki::Configuration do
-  context "defaults" do
-    let(:config) { Furoshiki::Configuration.new }
+  subject(:config) { Furoshiki::Configuration.new(raw_config) }
 
-    it "is valid" do
-      expect(config).to be_valid
+  let(:raw_config) { { warbler_extensions: OtherCustomization,
+                       ignore: "pkg" } }
+
+  class OtherCustomization < Furoshiki::WarblerExtensions
+    def customize(config)
+      # Actual behavior with shoes-package overrides the application name, so
+      # for testing purposes reset it here too.
+      config.pathmaps.application = ["shoes-app/%p"]
     end
+  end
+
+  it "is valid by default" do
+    expect(config).to be_valid
+  end
+
+  it "uses correct application path for exclusions" do
+    raw_config[:warbler_extensions] = OtherCustomization
+    raw_config[:ignore] = "pkg"
+
+    expect(config.to_warbler_config.excludes).to include("shoes-app/pkg")
   end
 end
