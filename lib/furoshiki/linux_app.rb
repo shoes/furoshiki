f79228464f7b280228503e7d590da994b96a1abd
Jason R. Clark <jason@jasonrclark.net>
2017-05-23 21:56:51 -0700

    Implement basic Linux app
    Just a shell script with the jar, but can download own JRE if needed.

---
+++ lib/furoshiki/linux_app.rb
@@ -0,0 +1,45 @@
+require 'furoshiki/base_app'
+require 'plist'
+
+module Furoshiki
+  class LinuxApp < BaseApp
+    private
+
+    def app_name
+      "#{config.name}-linux"
+    end
+
+    def template_basename
+      'linux-app-template'
+    end
+
+    def latest_template_version
+      '0.0.1'
+    end
+
+    def remote_template_url
+      "https://github.com/shoes/linux-app-templates/releases/download/v#{latest_template_version}/linux-app-template-#{latest_template_version}.zip"
+    end
+
+    def inject_jar
+      jar_path = ensure_jar_exists
+      cp Pathname.new(jar_path), File.join(tmp_app_path, "app.jar")
+    end
+
+    def after_built
+      mv File.join(tmp_app_path, "app"), File.join(tmp_app_path, config.name)
+    end
+
+    def after_moved
+      executable_path.chmod 0755
+    end
+
+    def executable_path
+      app_path.join(config.name)
+    end
+
+    def tmp_app_path
+      tmp.join "#{template_basename}"
+    end
+  end
+end
